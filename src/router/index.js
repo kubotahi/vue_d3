935144f108c69d13919ba318327a8b9b97cc7954
kubotahi <kub.web@gmail.com>
2020-08-20 18:17:18 +0900

    add

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Index from '../views/Index.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Index',
+    component: Index
+  },
+
+  //{
+  //  path: '/about',
+  //  name: 'About',
+  //  component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  //}
+
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <br>
+    Home
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+</script>
