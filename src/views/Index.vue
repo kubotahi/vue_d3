1368ece1318fe92b73f955540d404e642bfdb1e5
kubota <kub.web@gmail.com>
2020-10-15 09:31:02 +0900

    edit memo

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,14 +3,14 @@
 
     <div class="memo">
 
-      <h3>Development</h3>
+      <h3>Version</h3>
       vue/cli: 4.5.4<br>
       npm: 6.14.6<br>
       d3: 5.16.0<br>
       <br>
       <br>
 
-      <h3>Bible</h3>
+      <h3>Reference</h3>
       <a href="https://cli.vuejs.org/" target="_blank">https://cli.vuejs.org/</a><br>
       <a href="https://d3js.org/" target="_blank">https://d3js.org/</a><br>
       <a href="https://www.npmjs.com/package/d3" target="_blank">https://www.npmjs.com/package/d3</a><br>
